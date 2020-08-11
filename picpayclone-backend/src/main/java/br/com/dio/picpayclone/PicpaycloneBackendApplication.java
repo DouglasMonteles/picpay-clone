e48baa866e474415495917952532631c0a44a11d
DouglasMonteles <silvadouglas328@gmail.com>
2020-08-11 18:37:50 -0300

    Criando o projeto backend

---
+++ picpayclone-backend/src/main/java/br/com/dio/picpayclone/PicpaycloneBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.picpayclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaycloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaycloneBackendApplication.class, args);
+	}
+
+}
